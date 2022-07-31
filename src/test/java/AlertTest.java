305f3f11d54736bcadb984bf8a97f7202ed1eeeb
Paulina Lacinska <paulina.lacinska@gmail.com>
2022-07-31 15:26:18 +0200

    Initial commit

---
+++ src/test/java/AlertTest.java
@@ -0,0 +1,11 @@
+import org.testng.annotations.Test;
+
+public class AlertTest extends TestBase{
+
+    @Test
+    public void shouldFillFormWithSuccess(){
+
+    }
+}
+
+
---
+++ src/test/java/ForPopTest.java
@@ -0,0 +1,2 @@
+public class FormPopTest {
+}
